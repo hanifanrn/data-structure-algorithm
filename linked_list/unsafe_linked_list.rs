7cab761c426157f18b437778a8ab43b24c77b23e
hanifanrn <hanifanrizki@gmail.com>
2024-01-19 19:00:30 +0700

    finnised unsafe linked list in rust

--- linked_list/unsafe_linked_list.rs
+++ linked_list/unsafe_linked_list.rs
@@ -21,7 +21,7 @@ impl Node {
 struct LinkedList {
     head: *mut Node,
     tail: *mut Node,
-    length: i32,
+    length: usize,
 }
 
 impl LinkedList {
@@ -86,13 +86,13 @@ impl LinkedList {
     }
 
     pub fn delete_last(&mut self) {
-        let mut temp = self.head;
-
         if self.length == 0 {
+            return;
         } else if self.length == 1 {
             self.head = ptr::null_mut();
             self.tail = ptr::null_mut();
         } else {
+            let mut temp = self.head;
             let mut pre = self.head;
 
             unsafe {
@@ -107,9 +107,6 @@ impl LinkedList {
             unsafe {
                 (*self.tail).next = ptr::null_mut();
             }
-        }
-        unsafe {
-            drop(Box::from_raw(temp));
         }
 
         self.length -= 1;
@@ -130,6 +127,110 @@ impl LinkedList {
 
         self.length += 1;
     }
+
+    pub fn delete_first(&mut self) {
+        if self.length == 0 {
+        } else if self.length == 1 {
+            self.head = ptr::null_mut();
+            self.tail = ptr::null_mut();
+        } else {
+            unsafe {
+                self.head = (*self.head).next;
+            }
+        }
+
+        self.length -= 1;
+    }
+
+    pub fn get(&self, index: usize) -> *mut Node {
+        if index >= self.length {
+            return ptr::null_mut();
+        } else {
+            let mut temp = self.head;
+
+            for _i in 0..index {
+                unsafe {
+                    temp = (*temp).next;
+                }
+            }
+
+            return temp;
+        }
+    }
+
+    pub fn set(&mut self, index: usize, value: i32) -> bool {
+        let temp = self.get(index);
+
+        if !(temp.is_null()) {
+            unsafe {
+                (*temp).value = value;
+            }
+            return true;
+        }
+
+        return false;
+    }
+
+    pub fn insert(&mut self, index: usize, value: i32) -> bool {
+        if index > self.length {
+            return false;
+        }
+
+        if index == 0 {
+            self.prepend(value);
+            return true;
+        }
+
+        if index == self.length {
+            self.append(value);
+            return true;
+        }
+
+        let new_node = Node::new(value);
+        let temp = self.get(index - 1);
+
+        unsafe {
+            (*new_node).next = (*temp).next;
+            (*temp).next = new_node;
+        }
+
+        self.length += 1;
+        return true;
+    }
+
+    pub fn delete_node(&mut self, index: usize) {
+        if index >= self.length {
+            return;
+        } else if index == 0 {
+            self.delete_first();
+        } else if index == self.length - 1 {
+            self.delete_last();
+        } else {
+            let prev = self.get(index - 1);
+            unsafe {
+                let temp = (*prev).next;
+                (*prev).next = (*temp).next;
+            }
+
+            self.length -= 1;
+        }
+    }
+
+    pub fn reverse(&mut self) {
+        let mut temp = self.head;
+        self.head = self.tail;
+        self.tail = temp;
+        let mut before = ptr::null_mut();
+
+        unsafe {
+            for _i in 0..self.length {
+                let after = (*temp).next;
+                (*temp).next = before;
+                before = temp;
+                temp = after;
+            }
+        }
+    }
 }
 
 fn main() {
@@ -146,13 +247,55 @@ fn main() {
     my_linked_list.print_list();
     my_linked_list.get_length();
 
-    println!("delete list");
+    println!("delete last");
     my_linked_list.delete_last();
     my_linked_list.print_list();
     my_linked_list.get_tail();
+
+    println!("{:?}", my_linked_list);
+
+    let get_node_0 = my_linked_list.get(0);
+    let get_node_1 = my_linked_list.get(1);
+    let get_node_2 = my_linked_list.get(2);
+    let get_node_8 = my_linked_list.get(8);
+    println!(
+        "node 0: {:?} \n node 1: {:?} \n node 2: {:?} \n node 8: {:?}",
+        get_node_0, get_node_1, get_node_2, get_node_8
+    );
+
+    println!("changed {}", my_linked_list.set(4, 2));
 
     println!("prepend list");
     my_linked_list.prepend(9);
     my_linked_list.get_head();
     my_linked_list.print_list();
+
+    println!("new functionality");
+    my_linked_list.insert(2, 10);
+    my_linked_list.print_list();
+    println!("here work");
+    my_linked_list.delete_node(3);
+    my_linked_list.print_list();
+    my_linked_list.reverse();
+    my_linked_list.print_list();
+
+    println!("delete first");
+    my_linked_list.delete_first();
+    my_linked_list.print_list();
+
+    println!("deleting all node");
+    my_linked_list.delete_last();
+    my_linked_list.delete_last();
+    my_linked_list.delete_last();
+    println!("{:?}", my_linked_list);
+}
+
+#[cfg(test)]
+mod test {
+    use crate::LinkedList;
+    use crate::Node;
+
+    fn test_basic() {
+        let mut linked_list = LinkedList::new(0);
+    }
 }
